dab46ac8a763d4517a7d2bd8740b029ab1521f65
troppydash <troppydash.developer@gmail.com>
2026-02-05 15:48:29 +1300

    changed agents to use non blocking pipes

--- src/elo/agent.h
+++ src/elo/agent.h
@@ -16,13 +16,91 @@ struct agent_settings
     int m_tt_mb;
     bool m_verbose;
 
-    explicit agent_settings(std::string alias, std::string m_file, std::string m_nnue_file, std::string m_endgame_file,
-                            const int tt_mb, bool verbose = false)
+    explicit agent_settings(std::string alias, std::string m_file, std::string m_nnue_file,
+                            std::string m_endgame_file, const int tt_mb, bool verbose = false)
         : m_alias(std::move(alias)), m_file(std::move(m_file)), m_nnue_file(std::move(m_nnue_file)),
           m_endgame_file(std::move(m_endgame_file)), m_tt_mb(tt_mb), m_verbose(verbose)
     {
     }
 };
+
+namespace pipe_helpers
+{
+
+inline bool read_line(const bp::pipe &out, std::string &buffer, std::string &line)
+{
+    int fd = out.native_source();
+
+    char temp[4096];
+
+    while (true)
+    {
+        auto pos = buffer.find('\n');
+        if (pos != std::string::npos)
+        {
+            line = buffer.substr(0, pos);
+            buffer.erase(0, pos + 1);
+
+            if (!line.empty() && line.back() == '\r')
+                line.pop_back();
+
+            return true;
+        }
+
+        ssize_t n = read(fd, temp, sizeof(temp));
+        if (n == 0)
+            return false;
+        if (n < 0)
+        {
+            if (!(errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR))
+            {
+                perror("read_line");
+                return false;
+            }
+        }
+
+        if (n > 0)
+        {
+            buffer.append(temp, n);
+        }
+    }
+}
+
+inline bool write_line(const bp::pipe &in, const std::string &line)
+{
+    int fd = in.native_sink();
+
+    const char *data = line.data();
+    size_t remaining = line.size();
+
+    while (remaining > 0)
+    {
+        ssize_t n = write(fd, data, remaining);
+        if (n < 0)
+        {
+            if (!(errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR))
+            {
+                perror("write_line");
+                return false;
+            }
+        }
+        if (n > 0)
+        {
+            data += n;
+            remaining -= n;
+        }
+    }
+
+    return true;
+}
+
+inline void set_nonblocking(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
+} // namespace pipe_helpers
 
 /**
  * UCI agent wrapper
@@ -36,21 +114,29 @@ class agent
     std::string m_name;
 
     // process info
-    bp::ipstream m_out;
-    bp::opstream m_in;
+    bp::pipe m_out;
+    bp::pipe m_in;
     bp::child m_process;
 
   public:
-    explicit agent(const agent_settings &settings) : m_settings(settings), m_verbose(settings.m_verbose)
+    explicit agent(const agent_settings &settings)
+        : m_settings(settings), m_verbose(settings.m_verbose)
     {
         m_process = bp::child{settings.m_file, bp::std_out > m_out, bp::std_in < m_in};
 
+        pipe_helpers::set_nonblocking(m_in.native_sink());
+        pipe_helpers::set_nonblocking(m_out.native_source());
+
         // read uci
-        m_in << "uci" << std::endl;
-        std::string line;
+        pipe_helpers::write_line(m_in, "uci\n");
+
+        std::string line{};
+        std::string buffer{};
         int index = 0;
-        while (std::getline(m_out, line))
+        while (true)
         {
+            pipe_helpers::read_line(m_out, buffer, line);
+
             if (index == 0)
                 m_name = helper::string_split(line)[2] + " " + helper::string_split(line)[3];
 
@@ -64,14 +150,17 @@ class agent
         }
 
         // set options
-        m_in << "setoption name TTSizeMB value " << settings.m_tt_mb << std::endl;
-        m_in << "setoption name NNUEPath value " << settings.m_nnue_file << std::endl;
-        m_in << "setoption name SyzygyPath value " << settings.m_endgame_file << std::endl;
+        pipe_helpers::write_line(m_in, "setoption name TTSizeMB value " +
+                                           std::to_string(settings.m_tt_mb) + "\n");
+        pipe_helpers::write_line(m_in,
+                                 "setoption name NNUEPath value " + settings.m_nnue_file + "\n");
+        pipe_helpers::write_line(m_in, "setoption name SyzygyPath value " +
+                                           settings.m_endgame_file + "\n");
     }
 
     ~agent()
     {
-        m_in << "quit" << std::endl;
+        pipe_helpers::write_line(m_in, "quit\n");
         m_in.close();
         m_process.wait();
 
@@ -89,31 +178,40 @@ class agent
 
     void new_game()
     {
-        m_in << "ucinewgame" << std::endl;
+        pipe_helpers::write_line(m_in, "ucinewgame\n");
     }
 
-    chess::Move search(const std::vector<chess::Move> &moves, const search_param param, const int core = -1)
+    chess::Move search(const std::vector<chess::Move> &moves, const search_param param,
+                       const int core = -1)
     {
         // set core
-        m_in << "setoption name CoreAff value " << core << std::endl;
+        pipe_helpers::write_line(m_in,
+                                 "setoption name CoreAff value " + std::to_string(core) + "\n");
 
         // load position
         chess::Board board;
-        m_in << "position startpos moves";
+        pipe_helpers::write_line(m_in, "position startpos moves");
         for (const auto &move : moves)
         {
-            m_in << " " << chess::uci::moveToUci(move);
+            pipe_helpers::write_line(m_in, " " + chess::uci::moveToUci(move));
             board.makeMove(move);
         }
-        m_in << std::endl;
+        pipe_helpers::write_line(m_in, "\n");
 
         // search
-        m_in << "go depth " << param.depth << " movetime " << param.movetime << " wtime " << param.wtime << " btime "
-             << param.btime << " winc " << param.winc << " binc " << param.binc << std::endl;
+        std::string search_string =
+            "go depth " + std::to_string(param.depth) + " movetime " +
+            std::to_string(param.movetime) + " wtime " + std::to_string(param.wtime) + " btime " +
+            std::to_string(param.btime) + " winc " + std::to_string(param.winc) + " binc " +
+            std::to_string(param.binc) + "\n";
+        pipe_helpers::write_line(m_in, search_string);
 
         std::string line{};
-        while (std::getline(m_out, line))
+        std::string buffer{};
+        while (true)
         {
+            pipe_helpers::read_line(m_out, buffer, line);
+
             if (m_verbose)
                 std::cout << prefix() << line << std::endl;
 
