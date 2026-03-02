65665fd1b4fbd9efe3ad81aa04837ac383974913
Troppydash <troppydash.developer@gmail.com>
2026-03-02 23:39:36 +0800

    ver 1.5.24
    cleanedup ints
    added ttcapt in nmp
    increased zobrist 50mr

--- src/engine/util.h
+++ src/engine/util.h
@@ -16,9 +16,9 @@ inline void init() {
     std::uniform_int_distribution<uint64_t> dis;
 
     memset(ZOBRIST_50MR, 0, sizeof(ZOBRIST_50MR));
-    for (int i = 14; i <= 100; i += 8) {
+    for (int i = 14; i <= 100; i += 10) {
         uint64_t key = dis(gen);
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < 10; j++)
             ZOBRIST_50MR[i+j] = key;
     }
 }
